f1de322264a9e2809c292cadcd10c82432f2a3ef
RichAWarren <rwarren92@hotmail.com>
2020-02-12 21:11:00 +0000

    save

---
+++ next.config.js
@@ -0,0 +1,11 @@
+const withSass = require('@zeit/next-sass')
+const withTM = require('next-transpile-modules')
+
+module.exports = withSass(
+  withTM({
+    transpileModules: ['react-bulma-components'],
+    sassLoaderOptions: {
+      includePaths: ['./styles']
+    }
+  })
+)
